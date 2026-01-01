e8b8d5a99f90c1318901f0ef0ecf2b5f800f3fe8
Morgane WANKPO <wankpomorgane17@gmail.com>
2026-01-01 06:24:26 +0100

    Morgane

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,6 @@
 // backend/config/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require(Process.env.firebase_key); // Chemin vers ta clé Firebase Admin
+const serviceAccount = require(process.env.firebase_key); // Chemin vers ta clé Firebase Admin
 
 // Vérifie si l'app n'est pas déjà initialisée
 if (!admin.apps.length) {
