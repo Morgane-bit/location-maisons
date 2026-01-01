f46ace4930880202741829b83e3780de4d93a4ac
Morgane WANKPO <wankpomorgane17@gmail.com>
2026-01-01 06:12:05 +0100

    Correction firebaseAdmin.js : lecture du fichier JSON depuis .env

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,13 +1,15 @@
+// backend/config/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.firebase_key); // clé Firebase depuis .env
+const serviceAccount = require('../location-maison-476fb-firebase-adminsdk-fbsvc-680e2079e2.json'); // Chemin vers ta clé Firebase Admin
 
+// Vérifie si l'app n'est pas déjà initialisée
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 }
 
-const db = admin.firestore();
-const auth = admin.auth();
+const db = admin.firestore(); // Firestore
+const auth = admin.auth();    // Auth
 
 module.exports = { admin, db, auth };
--- test.js
+++
@@ -1,4 +0,0 @@
-const ioClient = require("socket.io-client");
-const socket = ioClient("http://localhost:5000");
-socket.on("connect", () => console.log("Connecté!"));
-socket.on("connect_error", (err) => console.log("Erreur:",err.message));
